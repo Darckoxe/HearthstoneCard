70a8783b583e9e06952bd8067533749994dd0717
Darckoxe <alexandredu_44@hotmail.fr>
2018-03-23 16:06:30 +0100

    Remplissage des spinners

--- app/src/main/java/com/example/taco/hearthstonecard/MainActivity.java
+++ app/src/main/java/com/example/taco/hearthstonecard/MainActivity.java
@@ -2,17 +2,26 @@ package com.example.taco.hearthstonecard;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -23,13 +32,88 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
-
+        final Spinner spinnerClasse = (Spinner) findViewById(R.id.spinnerClasse);
+        final Spinner spinnerType = (Spinner) findViewById(R.id.spinnerType);
+        final Spinner spinnerFaction = (Spinner) findViewById(R.id.spinnerFaction);
+        final Spinner spinnerRace = (Spinner) findViewById(R.id.spinnerRace);
+        final Spinner spinnerQualite = (Spinner) findViewById(R.id.spinnerQualite);
+        final Spinner spinnerSet = (Spinner) findViewById(R.id.spinnerSet);
         String url = "https://omgvamp-hearthstone-v1.p.mashape.com/info";
-
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
         JsonObjectRequest requeteInfos = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
-                /*on response*/
+                try {
+
+                    JSONArray classe = (JSONArray) response.get("classes");
+                    JSONArray type = (JSONArray) response.get("types");
+                    JSONArray faction = (JSONArray) response.get("factions");
+                    JSONArray race = (JSONArray) response.get("races");
+                    JSONArray qualite = (JSONArray) response.get("qualities");
+                    JSONArray set = (JSONArray) response.get("sets");
+
+                    ArrayList<String> listeClasse = new ArrayList<>();
+                    ArrayList<String> listeType = new ArrayList<>();
+                    ArrayList<String> listeFaction = new ArrayList<>();
+                    ArrayList<String> listeRace = new ArrayList<>();
+                    ArrayList<String> listeQualite = new ArrayList<>();
+                    ArrayList<String> listeSet = new ArrayList<>();
+
+                    if(classe != null){
+                        for (int i=0;i<classe.length();i++){
+                            listeClasse.add((String) classe.get(i));
+                        }
+                    }
+                    if(type != null){
+                        for (int i=0;i<type.length();i++){
+                            listeType.add((String) type.get(i));
+                        }
+                    }
+                    if(faction != null){
+                        for (int i=0;i<faction.length();i++){
+                            listeFaction.add((String) faction.get(i));
+                        }
+                    }
+                    if(race != null){
+                        for (int i=0;i<race.length();i++){
+                            listeRace.add((String) race.get(i));
+                        }
+                    }
+                    if(qualite != null){
+                        for (int i=0;i<qualite.length();i++){
+                            listeQualite.add((String) qualite.get(i));
+                        }
+                    }
+                    if(set != null){
+                        for (int i=0;i<set.length();i++){
+                            listeSet.add((String) set.get(i));
+                        }
+                    }
+                    ArrayAdapter<String> adaptClasse = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_dropdown_item, listeClasse);
+                    ArrayAdapter<String> adaptType = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_dropdown_item, listeType);
+                    ArrayAdapter<String> adaptFaction = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_dropdown_item, listeFaction);
+                    ArrayAdapter<String> adaptRace = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_dropdown_item, listeRace);
+                    ArrayAdapter<String> adaptQualite = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_dropdown_item, listeQualite);
+                    ArrayAdapter<String> adaptSet = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_dropdown_item, listeSet);
+
+
+                    spinnerClasse.setAdapter(adaptClasse);
+                    spinnerType.setAdapter(adaptType);
+                    spinnerFaction.setAdapter(adaptFaction);
+                    spinnerRace.setAdapter(adaptRace);
+                    spinnerQualite.setAdapter(adaptQualite);
+                    spinnerSet.setAdapter(adaptSet);
+
+                    spinnerClasse.setVisibility(View.VISIBLE);
+                    spinnerType.setVisibility(View.VISIBLE);
+                    spinnerFaction.setVisibility(View.VISIBLE);
+                    spinnerRace.setVisibility(View.VISIBLE);
+                    spinnerQualite.setVisibility(View.VISIBLE);
+                    spinnerSet.setVisibility(View.VISIBLE);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
             }
         }, new Response.ErrorListener() {
             @Override
@@ -46,5 +130,6 @@ public class MainActivity extends AppCompatActivity {
                 return headers;
             }
         };
+        requestQueue.add(requeteInfos);
     }
 }
